8be88146c61069105d4af522a7a5703d4486dadb
allbecarr <allbecarr@gmail.com>
2023-12-15 12:27:32 -0300

    intalação do Angular Materials

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,15 @@ import { RouterOutlet } from '@angular/router';
 import { Componente1Component } from "./componente1/componente1.component";
 import { Componente2Component } from "./componente2/componente2.component";
 
+/*  Angular Materials  */
+import {MatButtonModule} from '@angular/material/button';
+//=======================
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, Componente1Component, Componente2Component]
+    imports: [CommonModule, RouterOutlet, MatButtonModule,Componente1Component, Componente2Component]
 })
 export class AppComponent {
   title = 'projeto-angular';
